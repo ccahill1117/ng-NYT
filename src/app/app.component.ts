7632794ff8fe9cc26bc9babdcd513905eba25324
Christopher Cahill <cthomascahill@gmail.com>
2019-01-25 08:03:41 -0800

    initialize repo

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Faker } from 'faker';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'ny-times';
+
+  
+
+  // var randomName: string = Faker.fake.name.findName();
+
+
+}
